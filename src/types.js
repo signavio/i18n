daed36ff5b9a3c9a0986665e037538dd68e8e93b
Philipp Giese <giese.philipp+git@gmail.com>
2017-04-04 16:45:27 +0200

    added support for addLocation and noLocation options

---
+++ src/types.js
@@ -0,0 +1,15 @@
+export type AddLocationT = 'full' | 'file' | 'never';
+
+export type HeaderTypeT = '';
+
+export type HeadersT = {
+  [key: HeaderTypeT]: string,
+};
+
+export type ConfigT = {
+  functionName?: string,
+  fileName?: string,
+  headers?: HeadersT,
+  addLocation?: AddLocationT,
+  noLocation?: boolean,
+};
