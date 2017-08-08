b25a4444bd2e193ac4b62b850794bf6310c60b9e
Philipp Giese <giese.philipp+git@gmail.com>
2017-08-08 17:41:01 +0200

    added specs for custom babel config (#20)
    * added specs for custom babel config
    
    * removed extraneous export of ConfigT type
    
    * remove flow header

--- src/scripts/extract.js
+++ src/scripts/extract.js
@@ -1,18 +1,23 @@
+// @flow
 import glob from 'glob'
 
-import _ from 'lodash'
+import { each } from 'lodash'
 import { transformFileSync } from 'babel-core'
 import babelGettextExtractor from './babel-gettext-extractor'
 
 import getConfig from './config'
 
 if (process.argv.length < 3) {
-  throw new Error(`Invalid arguments, expected: 'node i18n/scripts/extract.js "source_file_pattern"', got: ${process.argv}`)
+  throw new Error(
+    `Invalid arguments, expected: 'node i18n/scripts/extract.js "source_file_pattern"', got: ${process.argv.join(
+      ' '
+    )}`
+  )
 }
 
 const files = glob.sync(process.argv[2])
 
-_.each(files, (fileName) => {
+each(files, (fileName: string) => {
   // eslint-disable-next-line no-console
   console.log(fileName)
 
@@ -21,9 +26,6 @@ _.each(files, (fileName) => {
 
   transformFileSync(fileName, {
     ...babel,
-    plugins: [
-      ...plugins,
-      [babelGettextExtractor, config],
-    ],
+    plugins: [...plugins, [babelGettextExtractor, config]],
   })
 })
--- src/types.js
+++ src/types.js
@@ -1,10 +1,10 @@
-export type AddLocationT = 'full' | 'file' | 'never';
+export type AddLocationT = 'full' | 'file' | 'never'
 
-export type HeaderTypeT = '';
+export type HeaderTypeT = ''
 
 export type HeadersT = {
   [key: HeaderTypeT]: string,
-};
+}
 
 export type ConfigT = {
   functionName?: string,
@@ -12,8 +12,12 @@ export type ConfigT = {
   headers?: HeadersT,
   addLocation?: AddLocationT,
   noLocation?: boolean,
-};
+}
 
 export type AstNodeT = {
   type: string,
-};
+}
+
+export type MapT<K, V> = {
+  [key: K]: V,
+}
